62e523eabc3f66a51b5b072c27d9e8fc1c65907c
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2021-08-01 20:54:06 -0500

    Database connection configured

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,10 +1,27 @@
+const db = require('mongoose')
+const Model = require('./model')
+
+db.Promise = global.Promise
+db.connect('mongodb+srv://CRESTREPO:dQy2bV7LMEnKySv@cluster0.e5xg7.mongodb.net/platziDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+
+console.log('Successfully connected to DB in mongoAtlas')
+
 const list = []
 
-const addMessage = message => list.push(message)
+const addMessage = message => {
+  const myMessage = new Model(message)
+  myMessage.save()
+}
 
-const getMessage = () => (list)
+const getMessages = async () => {
+    const messages = await Model.find()
+    return messages
+}
 
 module.exports = {
     add: addMessage,
-    list: getMessage
+    list: getMessages
 }
